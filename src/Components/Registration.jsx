bca9e81487c751d9fcb748205032129f06613f5c
M2K-5F <ivan.semjonow15@gmil.com>
2025-04-25 16:00:20 +0300

    a

--- src/Components/Registration.jsx
+++ src/Components/Registration.jsx
@@ -1,14 +1,15 @@
 export default function Regisration () {
     return(
-        <div className="auth-container">
+        <div className="registration-container">
             <h2>Регистрация</h2>
-            <form id="auth-form">
+            <form id="registration-form">
                 <input type="text" placeholder="login" className="input" autoComplete="off" datatype="off" name="login" />
                 <input type="text" placeholder="password" className="input" datatype="password" name="password" />
-                <div id="checkbox-container"> 
-                    <input id="remember" type='checkbox' />
-                    <label htmlFor="#remember"> Запомнить меня</label>
-                </div>
+                <input 
+                type="text"
+                placeholder="repeat password"
+                className="input"
+                name="password-rp" />
                 <button type='button' onClick={e => {}} id="button">Авторизоваться</button>
             </form>
         </div>
