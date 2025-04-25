a6467074f04cd9dd1cafb4df0b1e79870e527ef3
M2K-5F <ivan.semjonow15@gmil.com>
2025-04-25 16:05:46 +0300

    a

--- src/Components/Registration.jsx
+++ src/Components/Registration.jsx
@@ -4,7 +4,12 @@ export default function Regisration () {
             <h2>Регистрация</h2>
             <form id="registration-form">
                 <input type="text" placeholder="login" className="input" autoComplete="off" datatype="off" name="login" />
-                <input type="text" placeholder="password" className="input" datatype="password" name="password" />
+                <input 
+                type="text" 
+                placeholder="password" 
+                className="input" 
+                datatype="password" 
+                name="password" />
                 <input 
                 type="text"
                 placeholder="repeat password"
