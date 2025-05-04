6f9d40032d555ca180d10805a69ff09ed669892d
M2K-5F <ivan.semjonow15@gmail.com>
2025-05-04 10:52:18 +0300

    Button style update

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,10 @@
 import { createRoot } from 'react-dom/client' 
 import './styles/main.css'
 import RoutePaths from './Static/Routes'
+import  React from 'react'
 
 createRoot(document.getElementById('root')).render(
-  RoutePaths()
+  <React.StrictMode>
+    <RoutePaths />
+  </React.StrictMode>
 )
