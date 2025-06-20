52f43ef280a897febba672ceb67affd5c5361315
M2K-5F <ivan.semjonow15@gmail.com>
2025-06-20 13:20:40 +0300

    fix cookies validation

--- API/views.py
+++ API/views.py
@@ -33,17 +33,30 @@ async def validate_auth_user(
     return current_user
 
 
+
 async def get_current_user(
-    access_token: str|bytes = Cookie(None, include_in_schema=False),
-    bearer_token: HTTPAuthorizationCredentials = Depends(oauth2_scheme)
-) -> str:
+    access_token: str | bytes | None = Cookie(None, include_in_schema=False),
+    bearer_token: str | None = Depends(oauth2_scheme),
+) -> str | None:
     credentials_exception = HTTPException(
-        status_code=401,
+        status_code=status.HTTP_403_FORBIDDEN,
         detail="Could not validate credentials",
     )
+    if not access_token and not bearer_token:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED
+            )
+
     try:
-        payload = decode_jwt(token=access_token if access_token else bearer_token)
+        token = access_token or bearer_token
+        if token is None:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED
+            )
+
+        payload = decode_jwt(token=token)
         username: str = payload.get("sub")
+
         if username is None:
             raise credentials_exception
 
@@ -54,18 +67,17 @@ async def get_current_user(
 
 
 async def get_current_active_user(
-    username: str = Depends(get_current_user),
+    username: str = Depends(get_current_user)
 ) -> UserOut:
-    
+
     user = find_user(username)
 
     if not user:
         raise HTTPException(
-            status_code=403,
-            detail='cannot find user'
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail='could not find user'
         )
 
-    
     return user
 
 
